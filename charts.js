a95267d7e0a264798beffe907dc0ab1e1fcd3776
Santu Mahapatra <santumahapatra@gmail.com>
2015-07-07 18:13:33 +0530

    Cleaner format of setting attributes

--- charts.js
+++ charts.js
@@ -1,27 +1,26 @@
 var w = 300;
 var h = 100;
 var padding = 2;
-var dataset = [5, 10, 15, 20, 25];
+var dataset = [5, 10, 15, 20, 25, 13, 11, 29, 2];
 var svg = d3.select("body").append("svg")
           .attr("width", w)
-          .attr("height", h)
+          .attr("height", h);
+function colorPicker(v) {
+  if (v<=20) { return "#666666";}
+  else if (v>20) {return "#FF0033";}
+}
 
 svg.selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
-.attr("x", function(d, i){
-  return i * (w / dataset.length);
-})
-.attr("y", function(d){
-  return h - (d*4);
-})
-.attr("width", w / dataset.length - padding)
-.attr("height", function(d){
-  return d*4;
-})
-.attr("fill", "blue")
-
+.attr({
+  x: function(d, i){ return i * (w / dataset.length);},
+  y: function(d){ return h - (d*4);},
+  width: w / dataset.length - padding,
+  height: function(d) { return d*4;},
+  fill: function(d) { return colorPicker(d);}
+});
 
 
 
